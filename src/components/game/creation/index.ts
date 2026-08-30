7c57533416afba98f690262203c5c3aaa206dbcb
Pedro L. <pedrohmlabio@gmail.com>
2026-08-30 00:12:06 -0300

    feat(creation): add luminous collectible triggers

---
+++ src/components/game/creation/index.ts
@@ -0,0 +1,4 @@
+export { ColetaveisCriacao, RAIO_ATIVACAO_PADRAO } from './ColetaveisCriacao'
+export type { ColetaveisCriacaoProps } from './ColetaveisCriacao'
+export { distancia3D, estaNoRaio } from './proximidade'
+export type { PosicaoJogador } from './proximidade'
